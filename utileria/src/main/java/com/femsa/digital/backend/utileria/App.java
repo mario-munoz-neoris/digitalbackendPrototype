3c5b952737cd8ed87096a66f899bd7baf37e127c
Jesús Sánchez Cruz <jesus.scruz@neoris.com>
2022-12-20 17:15:59 -0600

    utileria

---
+++ utileria/src/main/java/com/femsa/digital/backend/utileria/App.java
@@ -0,0 +1,13 @@
+package com.femsa.digital.backend.utileria;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
