d9b019f854f1ce3aede21369f6053df9983b9e13
Jesús Sánchez Cruz <jesus.scruz@neoris.com>
2022-12-29 11:52:30 -0600

    Utils y logs

---
+++ utileria/src/main/java/com/femsa/digital/backend/utileria/UtileriaApplication.java
@@ -0,0 +1,13 @@
+package com.femsa.digital.backend.utileria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtileriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtileriaApplication.class, args);
+	}
+
+}
